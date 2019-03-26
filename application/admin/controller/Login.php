4c89ba26f52b0345edcad5a9b34550a47e2562c9
yangwenbing <770260332@qq.com>
2019-03-26 08:36:37 +0800

    项目初始化

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Login extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+
+    }
+
+    public function check() {
+        $data = input('post.');
+        if (!captcha_check($data['code'])) {
+            $this->error('验证码不正确');
+        }else{
+            $this->success('ok');
+        }
+
+    }
+
+    public function welcome(){
+        return "hello wordl";
+    }
+}
+
